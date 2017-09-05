657462dcaa0634dfbfc5d60b0b15476a9f87f646
Luka Ralic <luka.ralic@libertymutual.com>
2017-09-05 14:26:24 -0700

    initialized project

---
+++ src/main/java/com/libertymutual/goforcode/rolodex/RolodexApplication.java
@@ -0,0 +1,12 @@
+package com.libertymutual.goforcode.rolodex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RolodexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RolodexApplication.class, args);
+	}
+}
---
+++ src/test/java/com/libertymutual/goforcode/rolodex/RolodexApplicationTests.java
@@ -0,0 +1,16 @@
+package com.libertymutual.goforcode.rolodex;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RolodexApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
